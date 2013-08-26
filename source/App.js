bb0f1fc03209fac3211300254756f7fb48993490
Dataninja <dataninja@dataninja.it>
2013-08-26 19:38:54 +0200

    First working commit.

--- source/App.js
+++ source/App.js
@@ -3,15 +3,16 @@ enyo.kind({
 	kind: "FittableRows",
 	fit: true,
 	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
+		{kind: "onyx.Toolbar", content: "Calcio Mercato Estate 2013 - Tutti i trasferimenti della Serie A"},
 		{kind: "enyo.Scroller", fit: true, components: [
-			{name: "main", classes: "nice-padding", allowHtml: true}
+			{name: "main", classes: "nice-padding", allowHtml: true, components: [
+                {kind: "d3.Chord", fileNodes: "assets/cities.csv", fileMatrix: "assets/matrix.json", ontap: "tapped"}
+            ]}
 		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
-		]}
+		{kind: "onyx.Toolbar", content: "By Alessio 'jenkin' Cimarelli (@jenkin27) with Enyo Framework and D3 javascript library"}
 	],
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.addContent("The button was tapped.<br/>");
-	}
+    tapped: function(inS,inE) {
+        console.log(inS);
+        console.log(inE);
+    }
 });
