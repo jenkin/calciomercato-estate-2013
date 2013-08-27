41259397c27667bad9d4212ef58b0934355a82e4
Dataninja <dataninja@dataninja.it>
2013-08-27 17:43:45 +0200

    Final single chord with outgoing transfers.

--- source/App.js
+++ source/App.js
@@ -6,7 +6,8 @@ enyo.kind({
 		{kind: "onyx.Toolbar", content: "Calcio Mercato Estate 2013 - Tutti i trasferimenti della Serie A"},
 		{kind: "enyo.Scroller", fit: true, components: [
 			{name: "main", classes: "nice-padding", allowHtml: true, components: [
-                {kind: "d3.Chord", fileNodes: "assets/cities.csv", fileMatrix: "assets/matrix.json", ontap: "tapped"}
+                {kind: "d3.Chord", fileNodes: "assets/squadreA.csv", fileMatrix: "assets/cessioni.json", label: "Cessioni", ontap: "tapped"}
+                //{kind: "d3.Chord", fileNodes: "assets/squadreA.csv", fileMatrix: "assets/acquisti.json", label: "Acquisti", ontap: "tapped"}
             ]}
 		]},
 		{kind: "onyx.Toolbar", content: "By Alessio 'jenkin' Cimarelli (@jenkin27) with Enyo Framework and D3 javascript library"}
