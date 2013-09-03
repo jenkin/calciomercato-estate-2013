127aefd8070a7e2d85e26237931bd71034b9c90a
Dataninja <dataninja@dataninja.it>
2013-09-03 12:27:49 +0200

    Now control functions (nodes and chords labels, events, etc) of d3.Chord are customizable in App kind.

--- source/App.js
+++ source/App.js
@@ -7,11 +7,27 @@ enyo.kind({
 		{kind: "enyo.Scroller", fit: true, components: [
 			{name: "main", classes: "nice-padding", allowHtml: true, components: [
                 {tag: "h1", content: "Cessioni"},
-			    {name: "Chord", kind: "d3.Chord", label: "cessioni", nodes: squadreA, matrix: cessioni, details: trasferimenti}
+			    {name: "Chord", kind: "d3.Chord", onNodeLabel: "cessioninodelabel", onSelectChord: "onselectchord", nodes: squadreA, matrix: cessioni, details: trasferimenti}
                 //{tag: "h1", content: "Acquisti"},
 			    //{name: "Chord2", kind: "d3.Chord", label: "acquisti", nodes: squadreA, matrix: acquisti, details: trasferimenti}
             ]}
 		]},
 		{kind: "onyx.Toolbar", content: "By Alessio 'jenkin' Cimarelli (@jenkin27) with Enyo Framework and D3 javascript library | Powered by Dataninja | Source: Lega Serie A"}
-	]
+	],
+    cessioninodelabel: function(d,i) {
+        return this.nodes[i].name + ": " + Math.round(d.value) + " cessioni";
+    },
+    onselectchord: function(d,i) {
+        var that = this;
+        var a = this.details.filter(function(obj) { 
+                    return (
+                            obj["Squadra di provenienza"] === that.nodes[d.source.index].name 
+                            && obj["Squadra di destinazione"] === that.nodes[d.target.index].name
+                        ) || (
+                            obj["Squadra di provenienza"] === that.nodes[d.target.index].name 
+                            && obj["Squadra di destinazione"] === that.nodes[d.source.index].name
+                        );
+        });
+        if (a.length > 0) { console.log(a); }
+    }
 });
